b290760c0c0f0b8fa3bddf3429763eca407feb00
Tereso del Río <delriot@uni.coventry.ac.uk>
2022-10-28 12:30:55 +0100

    Model using tensorflow added

--- basic_ml.py
+++ basic_ml.py
@@ -1,4 +1,6 @@
 """Contains a function to do some basic machine learning."""
+import numpy as np
+from tensorflow import keras
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
@@ -42,8 +44,33 @@ def basic_ml(x_train, x_test, y_train, y_test, model, random_state=0):
             activation='logistic',
             max_iter=100000,
             random_state=random_state)
+    elif model == 'my_mlp':
+        return use_tf(x_train, x_test, y_train, y_test)
     else:
         raise Exception(f"No ML model called {model}")
 
     clf.fit(x_train, y_train)
     return accuracy_score(clf.predict(x_test), y_test)
+
+
+def use_tf(x_train, x_test, y_train, y_test, batch_size=64, epochs=100):
+    """Train a fully connected neural network and return its accuracy."""
+    # design model
+    model = keras.models.Sequential([
+        keras.layers.Dense(18, activation='relu',
+                           input_shape=(x_train.shape[1],)),
+        keras.layers.Dense(6),
+    ])
+    # choose hyperparameters
+    loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    optim = keras.optimizers.Adam(learning_rate=0.001)
+    metrics = ["accuracy"]
+    model.compile(loss=loss, optimizer=optim, metrics=metrics)
+
+    # convert the type to float so that the numpy arrays
+    # can be transformed into tensors
+    x_train = np.asarray(x_train).astype('float32')
+    x_test = np.asarray(x_test).astype('float32')
+
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+    return model.evaluate(x_test, y_test, batch_size=batch_size)
